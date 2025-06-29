629709980c6ba7077c8f9fb67beb9a9f07d17297
Rob N <rob.netzke@gmail.com>
2025-06-29 12:03:30 +0200

    accumulator: Refactor integration test
    Adds proper `assert` statements and removes the `println`

--- accumulator/tests/test.rs
+++ accumulator/tests/test.rs
@@ -1,17 +1,15 @@
-use std::{path::Path, time::Instant};
-
 use accumulator::Accumulator;
 use bitcoin::{OutPoint, Txid};
 use rusqlite::Connection;
 
 const SELECT_STMT: &str = "SELECT txid, vout FROM utxos";
 
-fn update_acc_from_outpoint_set<P: AsRef<Path>>(path: P, acc: &mut Accumulator) {
-    let conn = Connection::open(path).unwrap();
+#[test]
+fn test_static_utxo_set() {
+    let mut acc = Accumulator::new();
+    let conn = Connection::open("../contrib/signet_outpoints.sqlite").unwrap();
     let mut stmt = conn.prepare(SELECT_STMT).unwrap();
     let mut rows = stmt.query([]).unwrap();
-    let now = Instant::now();
-    println!("Spending UTXOS from accumulator...");
     while let Some(row) = rows.next().unwrap() {
         let txid: String = row.get(0).unwrap();
         let vout: u32 = row.get(1).unwrap();
@@ -19,12 +17,9 @@ fn update_acc_from_outpoint_set<P: AsRef<Path>>(path: P, acc: &mut Accumulator) 
         let outpoint = OutPoint { txid, vout };
         acc.spend(outpoint);
     }
-    println!("Done spending UTXOs in {} seconds", now.elapsed().as_secs());
-    println!("Accumulator is zero: {}", acc.is_zero());
+    assert!(!acc.is_zero());
     let mut stmt = conn.prepare(SELECT_STMT).unwrap();
     let mut rows = stmt.query([]).unwrap();
-    let now = Instant::now();
-    println!("Adding UTXOS to accumulator...");
     while let Some(row) = rows.next().unwrap() {
         let txid: String = row.get(0).unwrap();
         let vout: u32 = row.get(1).unwrap();
@@ -32,12 +27,5 @@ fn update_acc_from_outpoint_set<P: AsRef<Path>>(path: P, acc: &mut Accumulator) 
         let outpoint = OutPoint { txid, vout };
         acc.add(outpoint);
     }
-    println!("Done adding UTXOs in {} seconds", now.elapsed().as_secs());
-    println!("Accumulator is zero: {}", acc.is_zero());
-}
-
-#[test]
-fn test_static_utxo_set() {
-    let mut acc = Accumulator::new();
-    update_acc_from_outpoint_set("../contrib/signet_outpoints.sqlite", &mut acc);
+    assert!(acc.is_zero());
 }
