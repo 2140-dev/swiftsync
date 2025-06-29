88e9ddb187924d6aed0c436c2fe02fdd97ac7f2b
Rob N <rob.netzke@gmail.com>
2025-06-29 12:15:16 +0200

    verifier: Remove shared lock on the accumulator
    The shared lock on the accumulator between the block download tasks
    and the blocking database task that loads UTXOs from sqlite is a
    performance drag.
    
    This refactors the verification assumption to say the accumulator state
    of the UTXO set should be the same as the accumulator state after doing
    block download to the assume valid hash.

--- utxo_verifier/src/loader.rs
+++ utxo_verifier/src/loader.rs
@@ -1,16 +1,16 @@
-use std::{path::Path, sync::Arc, time::Instant};
+use std::{path::Path, time::Instant};
 
 use accumulator::Accumulator;
 use bitcoin::{OutPoint, Txid};
 use std::collections::BTreeMap;
-use tokio::sync::Mutex;
 
 use bitcoin::{consensus, BlockHash};
 use rusqlite::Connection;
 
 const SELECT_STMT: &str = "SELECT txid, vout FROM utxos";
 
-pub fn update_acc_from_outpoint_set<P: AsRef<Path>>(path: P, acc: Arc<Mutex<Accumulator>>) {
+pub fn update_acc_from_outpoint_set<P: AsRef<Path>>(path: P) -> Accumulator {
+    let mut acc = Accumulator::new();
     let conn = Connection::open(path).unwrap();
     let mut stmt = conn.prepare(SELECT_STMT).unwrap();
     let mut rows = stmt.query([]).unwrap();
@@ -22,14 +22,14 @@ pub fn update_acc_from_outpoint_set<P: AsRef<Path>>(path: P, acc: Arc<Mutex<Accu
         let vout: u32 = row.get(1).unwrap();
         let txid = txid.parse::<Txid>().unwrap();
         let outpoint = OutPoint { txid, vout };
-        let mut acc = acc.blocking_lock();
-        acc.spend(outpoint);
+        acc.add(outpoint);
         outpoints_spent += 1;
         if outpoints_spent % 1_000_000 == 0 {
             tracing::info!("{outpoints_spent} OutPoints added to the accumulator");
         }
     }
     tracing::info!("Done spending UTXOs in {} seconds", now.elapsed().as_secs());
+    acc
 }
 
 pub fn get_block_hashes_from_store<P: AsRef<Path>>(
