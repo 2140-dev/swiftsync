aa7fb13dc72b0ccc2ac47e4bace048d62437cbec
rustaceanrob <rob.netzke@gmail.com>
2025-08-15 14:00:25 +0100

    Add initial `node` format

--- node/src/bin/ibd.rs
+++ node/src/bin/ibd.rs
@@ -1,1 +1,44 @@
-fn main() {}
+use std::{fs::File, sync::Arc, time::Instant};
+
+use bitcoin::Network;
+use hintfile::Hints;
+use kernel::{ChainType, ChainstateManager, ChainstateManagerOptions, ContextBuilder};
+
+use node::{bootstrap_dns, elapsed_time};
+
+const CHAIN_TYPE: ChainType = ChainType::SIGNET;
+const NETWORK: Network = Network::Signet;
+
+fn main() {
+    let mut args = std::env::args();
+    let _ = args.next();
+    let hint_path = args
+        .next()
+        .expect("Usage: <path_to_hints_file>");
+    // Logging
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+    let hintfile_start_time = Instant::now();
+    tracing::info!("Reading in {hint_path}");
+    let mut hintfile = File::open(hint_path).expect("invalid hintfile path");
+    let hints = Arc::new(Hints::from_file(&mut hintfile));
+    elapsed_time(hintfile_start_time);
+    let stop_hash = hints.stop_hash();
+    tracing::info!("Assume valid hash: {stop_hash}");
+    tracing::info!("Finding peers with DNS");
+    let dns_start_time = Instant::now();
+    let peers = bootstrap_dns(NETWORK);
+    elapsed_time(dns_start_time);
+    tracing::info!("Initializing bitcoin kernel");
+    let kernel_start_time = Instant::now();
+    let ctx = ContextBuilder::new()
+        .chain_type(CHAIN_TYPE)
+        .build()
+        .unwrap();
+    let options = ChainstateManagerOptions::new(&ctx, ".", "./blocks").unwrap();
+    let _context = Arc::new(ctx);
+    let chainman = ChainstateManager::new(options).unwrap();
+    elapsed_time(kernel_start_time);
+    let tip = chainman.best_header().height();
+    tracing::info!("Kernel best header: {tip}");
+}
--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,1 +1,59 @@
+use std::{net::SocketAddr, sync::mpsc::Receiver, time::Instant};
 
+use accumulator::{Accumulator, AccumulatorUpdate};
+use bitcoin::Network;
+use network::dns::DnsQuery;
+use p2p::SeedsExt;
+
+pub fn elapsed_time(then: Instant) {
+    let duration_sec = then.elapsed().as_secs_f64();
+    tracing::info!("Elapsed time {duration_sec} seconds");
+}
+
+pub trait PortExt {
+    fn port(&self) -> u16;
+}
+
+impl PortExt for Network {
+    fn port(&self) -> u16 {
+        match self {
+            Network::Signet => 38333,
+            Network::Bitcoin => 8333,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AccumulatorState {
+    acc: Accumulator,
+    update_rx: Receiver<AccumulatorUpdate>,
+}
+
+impl AccumulatorState {
+    fn new(rx: Receiver<AccumulatorUpdate>) -> Self {
+        Self {
+            acc: Accumulator::new(),
+            update_rx: rx,
+        }
+    }
+
+    fn verify(&mut self) -> bool {
+        while let Ok(update) = self.update_rx.recv() {
+            self.acc.update(update);
+        }
+        self.acc.is_zero()
+    }
+}
+
+pub fn bootstrap_dns(network: Network) -> Vec<SocketAddr> {
+    let mut all_hosts = Vec::new();
+    for seed in network.seeds() {
+        let hosts = DnsQuery::new_cloudflare(seed).lookup().unwrap_or_default();
+        all_hosts.extend_from_slice(&hosts);
+    }
+    all_hosts
+        .into_iter()
+        .map(|host| SocketAddr::new(host, network.port()))
+        .collect()
+}
