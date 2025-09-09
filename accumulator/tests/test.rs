33a924173628107617cc875dab43c3297010559e
rustaceanrob <rob.netzke@gmail.com>
2025-09-09 12:12:21 +0100

    acc: Add a multiplication-based aggregate
    Precompute a table of `vout` to hash to add some additional salt, and
    add/spend outpoints with simple multiplications and adds

--- accumulator/tests/test.rs
+++ accumulator/tests/test.rs
@@ -1,4 +1,4 @@
-use accumulator::Accumulator;
+use accumulator::{Accumulator, MultAggregate};
 use bitcoin::{OutPoint, Txid};
 use rusqlite::Connection;
 
@@ -7,6 +7,32 @@ const SELECT_STMT: &str = "SELECT txid, vout FROM utxos";
 #[test]
 fn test_static_utxo_set() {
     let mut acc = Accumulator::new();
+    let conn = Connection::open("../contrib/data/signet_outpoints.sqlite").unwrap();
+    let mut stmt = conn.prepare(SELECT_STMT).unwrap();
+    let mut rows = stmt.query([]).unwrap();
+    while let Some(row) = rows.next().unwrap() {
+        let txid: String = row.get(0).unwrap();
+        let vout: u32 = row.get(1).unwrap();
+        let txid = txid.parse::<Txid>().unwrap();
+        let outpoint = OutPoint { txid, vout };
+        acc.spend(outpoint);
+    }
+    assert!(!acc.is_zero());
+    let mut stmt = conn.prepare(SELECT_STMT).unwrap();
+    let mut rows = stmt.query([]).unwrap();
+    while let Some(row) = rows.next().unwrap() {
+        let txid: String = row.get(0).unwrap();
+        let vout: u32 = row.get(1).unwrap();
+        let txid = txid.parse::<Txid>().unwrap();
+        let outpoint = OutPoint { txid, vout };
+        acc.add(outpoint);
+    }
+    assert!(acc.is_zero());
+}
+
+#[test]
+fn test_mult_agg() {
+    let mut acc = MultAggregate::new();
     let conn = Connection::open("../contrib/data/signet_outpoints.sqlite").unwrap();
     let mut stmt = conn.prepare(SELECT_STMT).unwrap();
     let mut rows = stmt.query([]).unwrap();
